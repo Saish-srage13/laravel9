47c4f4cac00b99435871815937c6f0f0ce0fe3f3
Saish <Saish SSK>
2023-10-22 16:58:49 +0530

    Uncomments test cases

--- tests/Feature/SendingEmailTest.php
+++ tests/Feature/SendingEmailTest.php
@@ -16,86 +16,86 @@ class SendingEmailTest extends TestCase
      *
      * @return void
      */
-    // public function test_application_throws_a_validation_error_and_does_not_queue_mail_if_the_request_object_is_incorrect_or_malformed()
-    // {
-    //     Mail::fake();
+    public function test_application_throws_a_validation_error_and_does_not_queue_mail_if_the_request_object_is_incorrect_or_malformed()
+    {
+        Mail::fake();
 
-    //     Queue::fake();
+        Queue::fake();
 
-    //     $response = $this->post('/api/token/send', [
-    //         "emails" => [
-    //             [
-    //                 "to_email" => "testemail1@email.com",
-    //                 "subject" => "Subject 1",
-    //                 "body" => "Body 1"
-    //             ],
-    //             [
-    //                 "subject" => "Subject 2",
-    //                 "body" => "Body 2"
-    //             ]
-    //         ]
-    //     ]);
+        $response = $this->post('/api/token/send', [
+            "emails" => [
+                [
+                    "to_email" => "testemail1@email.com",
+                    "subject" => "Subject 1",
+                    "body" => "Body 1"
+                ],
+                [
+                    "subject" => "Subject 2",
+                    "body" => "Body 2"
+                ]
+            ]
+        ]);
 
-    //     $response
-    //         ->assertStatus(400)
-    //         ->assertJson([
-    //             "status" => false,
-    //             "message" => "To email is required",
-    //             "data" => [
-    //                 "errors" => [
-    //                     "To email is required"
-    //                 ]
-    //             ]
-    //         ]);
+        $response
+            ->assertStatus(400)
+            ->assertJson([
+                "status" => false,
+                "message" => "To email is required",
+                "data" => [
+                    "errors" => [
+                        "To email is required"
+                    ]
+                ]
+            ]);
 
-    //     Queue::assertNotPushed(HandleEmailDelivery::class);
+        Queue::assertNotPushed(HandleEmailDelivery::class);
 
-    //     /*
-    //         These to assets were incase the Mailable class was directly being queued instead of have a job 
+        /*
+            These to assets were incase the Mailable class was directly being queued instead of have a job 
 
-    //         Mail::assertNotQueued(SendEmail::class);
+            Mail::assertNotQueued(SendEmail::class);
 
-    //     */
-    // }
+        */
+    }
 
-    // public function test_mail_is_queued_up_if_request_body_is_valid()
-    // {
-    //     Mail::fake();
+    public function test_mail_is_queued_up_if_request_body_is_valid()
+    {
+        Mail::fake();
 
-    //     Queue::fake();
+        Queue::fake();
 
-    //     $response = $this->post('/api/token/send', [
-    //         "emails" => [
-    //             [
-    //                 "to_email" => "testemail1@email.com",
-    //                 "subject" => "Subject 1",
-    //                 "body" => "Body 1"
-    //             ],
-    //             [
-    //                 "to_email" => "testemail2@email.com",
-    //                 "subject" => "Subject 2",
-    //                 "body" => "Body 2"
-    //             ]
-    //         ]
-    //     ]);
+        $response = $this->post('/api/token/send', [
+            "emails" => [
+                [
+                    "to_email" => "testemail1@email.com",
+                    "subject" => "Subject 1",
+                    "body" => "Body 1"
+                ],
+                [
+                    "to_email" => "testemail2@email.com",
+                    "subject" => "Subject 2",
+                    "body" => "Body 2"
+                ]
+            ]
+        ]);
 
-    //     $response->assertStatus(200)
-    //         ->assertJson([
-    //             "status" => true,
-    //             "message" => "Email's queued successfully!",
-    //             "data" => []
-    //         ]);
+        $response->assertStatus(200)
+            ->assertJson([
+                "status" => true,
+                "message" => "Email's queued successfully!",
+                "data" => []
+            ]);
 
-    //     Queue::assertPushed(HandleEmailDelivery::class);
+        Queue::assertPushed(HandleEmailDelivery::class);
         
-    //     /*
-    //         These to assets were incase the Mailable class was directly being queued instead of have a job 
+        /*
+            These to assets were incase the Mailable class was directly being queued instead of have a job 
 
-    //         Mail::assertQueued(SendEmail::class);
+            Mail::assertQueued(SendEmail::class);
 
-    //         Mail::assertQueuedCount(2);
+            Mail::assertQueuedCount(2);
         
-    //     */
-    // }
+        */
+    }
 
 }
